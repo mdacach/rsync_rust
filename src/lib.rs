f38d5ee6c861393ad6f747ac44ad6b8e3e8ce80b
Matheus Cardoso <dacachmatheus@gmail.com>
2023-01-27 19:35:43 -0300

    Add clap for cli parsing
    Signed-off-by: Matheus Cardoso <dacachmatheus@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+use bytes::Bytes;
+
+// Use the default hash is std for now
+fn calculate_hash(content: &[u8]) -> u64 {
+    let mut s = DefaultHasher::new();
+    content.hash(&mut s);
+    s.finish()
+}
+
+fn compute_signature(content: Bytes, chunk_size: usize) -> Vec<u64> {
+    let blocks = content.chunks(chunk_size);
+    blocks.map(calculate_hash).collect()
+}
+
+
+#[test]
+fn equal_contents_have_equal_signatures() {
+    let left = Bytes::from("ABCDEFGH");
+    let right = Bytes::from("ABCDEFGH");
+    let chunk_size = 4;
+    let left_signature = compute_signature(left, chunk_size);
+    let right_signature = compute_signature(right, chunk_size);
+    assert_eq!(left_signature, right_signature);
+}
+
+#[test]
+fn different_contents_have_different_signatures() {
+    let left = Bytes::from("ABCDEFGH");
+    let right = Bytes::from("AB");
+    let chunk_size = 4;
+    let left_signature = compute_signature(left, chunk_size);
+    let right_signature = compute_signature(right, chunk_size);
+    assert_ne!(left_signature, right_signature);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,28 @@
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
+use clap::{Parser, Subcommand};
 
-use bytes::Bytes;
+#[derive(Parser)]
+struct Arguments {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Signature {
+        file: String,
+        output_file: String,
+    },
+    Delta,
+    Patch,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::parse();
+
+    match args.command {
+        Commands::Signature { file, output_file } => {},
+        Commands::Delta => println!("Delta"),
+        Commands::Patch => println!("Patch"),
+    }
 }
 
-// Use the default hash is std for now
-fn calculate_hash(content: &[u8]) -> u64 {
-    let mut s = DefaultHasher::new();
-    content.hash(&mut s);
-    s.finish()
-}
-
-fn compute_signature(content: Bytes, chunk_size: usize) -> Vec<u64> {
-    let blocks = content.chunks(chunk_size);
-    blocks.map(calculate_hash).collect()
-}
-
-
-#[test]
-fn equal_contents_have_equal_signatures() {
-    let left = Bytes::from("ABCDEFGH");
-    let right = Bytes::from("ABCDEFGH");
-    let chunk_size = 4;
-    let left_signature = compute_signature(left, chunk_size);
-    let right_signature = compute_signature(right, chunk_size);
-    assert_eq!(left_signature, right_signature);
-}
-
-#[test]
-fn different_contents_have_different_signatures() {
-    let left = Bytes::from("ABCDEFGH");
-    let right = Bytes::from("AB");
-    let chunk_size = 4;
-    let left_signature = compute_signature(left, chunk_size);
-    let right_signature = compute_signature(right, chunk_size);
-    assert_ne!(left_signature, right_signature);
-}
