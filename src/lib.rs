d6e52520dc093eb6e3a0d6fd0a4c6d66d5993707
Matheus Cardoso <dacachmatheus@gmail.com>
2023-02-01 14:21:37 -0300

    Update `write_to_file` to create the file if needed.
    Signed-off-by: Matheus Cardoso <dacachmatheus@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ pub fn read_file<P: AsRef<Path>>(path: P) -> color_eyre::Result<Bytes> {
 }
 
 pub fn write_to_file<P: AsRef<Path>>(path: P, content: Bytes) -> color_eyre::Result<()> {
-    let mut file = File::open(path)?;
+    let mut file = File::create(path)?;
     file.write_all(&content)?;
 
     Ok(())
